dc14e3577cf5b8f1ca2501bc4898e000ff9ca338
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-02-17 05:50:10 +0100

    all cards added

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Game } from './../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -9,9 +10,19 @@ import { CommonModule } from '@angular/common';
 })
 export class GameComponent {
   pickCardAnimation = false;
+  game!: Game;
+
+ngOnInit(): void {
+  this.newGame();
+}
 
 takeCard(){
   this.pickCardAnimation = true;
+}
+
+newGame() {
+  this.game = new Game();
+  console.log(this.game);
 }
 
 }
