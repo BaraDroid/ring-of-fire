a43ba87ae82b686c76bde3371b686c9eae4c6ba2
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-02-17 06:03:22 +0100

    function shuffle added for mixin the card stack

--- src/models/game.ts
+++ src/models/game.ts
@@ -11,7 +11,25 @@ export class Game {
             this.stack.push('clubs_' + i);
             this.stack.push('diamonds_' + i);
         }
-        
+        shuffle(this.stack);
     }
 
 }
+
+function shuffle(array: string[]) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
